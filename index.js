4cb0caf79cf8accefe9137a2a16df7a954b0e802
Squirrelcoding <68344468+Squirrelcoding@users.noreply.github.com>
2020-11-17 18:04:26 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+var verify = require('./easyCaptcha.js')
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+function correct() {
+  console.log('Verified!')
+}
+function incorrect() {
+  console.log('Incorrect Code')
+}
+var email = process.env.EMAIL;
+var password = process.env.PASSWORD;
+verify.authentication(email, password); //Alt account
+rl.question("Enter your email:", ans => {
+  verify.sendCode(ans)
+  rl.question("Enter the Code we sent you :", ans => {
+    verify.verify(ans, correct, incorrect)
+  })
+})
